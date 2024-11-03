fc90a43958a88945e95d152270e176c14f89090b
Chonrathan Sinlapachan <namchonrathan@gmail.com>
2024-11-03 22:37:17 +0700

    5th commit: JS Function Logout

--- js/script.js
+++ js/script.js
@@ -1,27 +1,15 @@
-function goToPage(type) {
-    
-    switch(type) {
-        case 1:
-            window.location.href = "form1.html";
-            break;
-        case 2:
-            window.location.href = "form2.html";
-            break;
-        case 3:
-            window.location.href = "form3.html";
-            break;
-        case 4:
-            window.location.href = "form4.html";
-            break;
-        case 5:
-            window.location.href = "form5.html";
-            break;
-        default:
-            alert("ประเภทคำร้องไม่ถูกต้อง");
-    }
-}
-
 document.addEventListener('DOMContentLoaded', function() {
+
+    const  logoutButton = document.querySelector('.logout');
+    logoutButton.addEventListener('click', function() {
+        
+        if (confirm('คุณต้องการออกจากระบบหรือไม่?')) {
+          
+            document.querySelectorAll('input').forEach(input => input.value = '');
+        }
+
+        window.location.href = 'index.html';
+    });
 
     const exampleButton = document.querySelector('.example');
     exampleButton.addEventListener('click', function() {
